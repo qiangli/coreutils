e38171bf73ed8a34076695515903123cf4e40553
Qiang Li <liqiang@gmail.com>
2026-08-27 02:07:05 -0700

    fix(cp): preserve full-range symlink timestamps

--- cmds/cp/link_times_unix.go
+++ cmds/cp/link_times_unix.go
@@ -11,8 +11,8 @@ import (
 // preserveLinkTimes updates the link inode, never its referent.
 func preserveLinkTimes(path string, atime, mtime time.Time) error {
 	times := []unix.Timespec{
-		unix.NsecToTimespec(atime.UnixNano()),
-		unix.NsecToTimespec(mtime.UnixNano()),
+		{Sec: atime.Unix(), Nsec: int64(atime.Nanosecond())},
+		{Sec: mtime.Unix(), Nsec: int64(mtime.Nanosecond())},
 	}
 	return unix.UtimesNanoAt(unix.AT_FDCWD, path, times, unix.AT_SYMLINK_NOFOLLOW)
 }
